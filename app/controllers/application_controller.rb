4db927918b712a3a85f7b2b9ec8427d10cf433ae
55ponsuke0123 <74514503+55ponsuke0123@users.noreply.github.com>
2021-03-29 10:46:42 +0900

    ユーザー新規登録

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,17 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  
+
   private
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:child_name, :classroom, :childminder1, :childminder2, :childminder3])
   end
 end
---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,16 @@
+class BooksController < ApplicationController
+  #before_action :authenticate_user!, except: [:index]
+  
+  def index
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+  end
+
+  def show
+  end
+end
