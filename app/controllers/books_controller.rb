676f44a513ebd590b3fb2ccfdea130b0cefbbd5e
55ponsuke0123 <74514503+55ponsuke0123@users.noreply.github.com>
2021-03-29 16:30:01 +0900

    大まかな完成２

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,5 @@
 class BooksController < ApplicationController
-  #before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, except: [:index]
   
   def index
   end
@@ -9,6 +9,18 @@ class BooksController < ApplicationController
   end
 
   def create
+    @book = Book.new(book_params)
+    if @book.save
+      render :create
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def book_params
+    params.permit(:presence_id, :mood_id, :body_temperature_id, :sleep_id, :breakfast, :comment1, :reason_id, :period_id, :comment2).merge(user_id: current_user.id)
   end
 
 end
